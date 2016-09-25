0f396c7b84e0ebdc558eded76b4f0bfee281966a
Doyle Fermi <doylefermi@gmail.com>
2016-09-25 23:13:33 +0530

    Added md5 check for timeout and outofmemory
    {'result': 'Testcase input: \nYour output: Out of memory. Warning!!! You
    will be reported to the admin', 'md5':
    'b51abcddf693c69824cc5f262f68084b'}
    
    {'result': 'Testcase input: Hello world\n\nYour output: Timedout.
    Warning!!! You will be reported to the admin', 'md5':
    'b498eb642d47b33c5e268625751cb062'}

--- binbash/src/bashbin/docker/test_program.py
+++ binbash/src/bashbin/docker/test_program.py
@@ -3,10 +3,10 @@ import os
 import signal
 from bashmain import check_code
 
-for i in xrange(2,3):
+for i in xrange(1,2):
     dir = "/home/haxorware/bash_q/Level1/Question"+str(i)+"/"
     user_answer = dir+'answer.sh'
     extrafiles = [dir+'file.txt',dir+'.file2.txt',dir+'.file3.txt']
     testcases = [dir + 'testcase.txt',dir+'.testcase2.txt',dir+'.testcase3.txt']
     print dir
-    print check_code(user_answer, extrafiles, testcases, "/tmp")
+    print check_code(user_answer, extrafiles, testcases, "/tmp/binbash")
