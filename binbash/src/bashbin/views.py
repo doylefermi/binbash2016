635c2ba790ecb5ab112f90ee315c429965335fd8
Doyle Fermi <doylefermi@gmail.com>
2016-09-28 10:12:07 +0530

    Added newline

--- binbash/src/bashbin/views.py
+++ binbash/src/bashbin/views.py
@@ -204,7 +204,7 @@ def submit_request(user_id, answer_path):
         current_user.cat_of_answer = ""
         current_user.save()
         context["result"] = "Success on test cases\n" + str(context["result"]).replace("52.39.25.19", "Nigga you thought you can find me? _!_")
-        context["info"] = "You have successfully cleared this round."
+        context["info"] = "You have successfully cleared this round.\n\n"
         if  current_user.question == 1 :
             Qobject = Question.objects.get(question_id=current_user.question, level_id=current_user.level)
             context["info"] +=  Qobject.intro_to_level
